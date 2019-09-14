4cde6b628c9f3b25ccb1165f0ae033f7768b3806
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2019-09-13 19:21:44 -0700

    Update and rename Quiz 2 - Gradient Descent Algorithm to quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,60 @@
+# Quiz 2 - Gradient Descent Algorithm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def partial_0_J(a, b):
+    J = 0
+    for i in data:
+        J += (a + b * i[0] - i[1])
+    J /=  len(data)
+    return J
+
+def partial_1_J(a, b):
+    J = 0
+    for i in data:
+        J += (a + b * i[0] - i[1]) * i[0]
+    J /=  len(data)
+    return J
+
+def J(a, b):
+        temp = 0
+        for i in data:
+                temp += (a + b * i[0] - i[1]) ** 2
+        return temp / (2 * len(data))
+
+def main():
+    data = np.array([[0.5, 0.9], [0.3, 0.7], [1.1, 2.3], [2.0, 4.3], [3.5, 6.8], [4.1, 8], [0, 0.1], [5.8, 11]])
+
+    theta_0 = np.arange(-3, 5, 0.1)
+    theta_1 = np.arange(0, 3, 0.1)
+    X, Y = np.meshgrid(theta_0, theta_1)
+    theta_0 = 0
+    theta_1 = 0
+    alpha = 0.1
+    min_dif = 0.01
+
+    Z= J(X, Y)
+    prev_J = J(theta_0, theta_1)
+    temp = theta_0
+    theta_0 = theta_0 - alpha * partial_0_J(theta_0, theta_1)
+    theta_1 = theta_1 - alpha * partial_1_J(temp, theta_1)
+    current_J = J(theta_0, theta_1)
+
+    surf = plt.contour(X, Y, Z, 40)
+
+    while (current_J - prev_J) > min_dif:
+        prev_J = current_J
+        temp = theta_0
+        theta_0 = theta_0 - alpha * partial_0_J(theta_0, theta_1)
+        theta_1 = theta_1 - alpha * partial_1_J(temp, theta_1)
+        current_J = J(theta_0, theta_1)
+        plt.plot(theta_0, theta_1)
+
+    plt.show()
+    print(theta_0)
+    print(theta_1)
+
+if __name__ == "__main__":
+        main()
