df5baa633535b9ae9ce70899a94c89a58c0d1ee7
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2019-09-18 19:40:55 -0700

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -4,6 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+# error function
+def J(a, b):
+        temp = 0
+        for i in data:
+                temp += (a + b * i[0] - i[1]) ** 2
+        return temp / (2 * len(data))
+
+# partial derivative of J with respect to theta_0
 def partial_0_J(a, b):
     J = 0
     for i in data:
@@ -11,6 +19,7 @@ def partial_0_J(a, b):
     J /=  len(data)
     return J
 
+# partial derivative of J with respect to theta_1
 def partial_1_J(a, b):
     J = 0
     for i in data:
@@ -18,15 +27,10 @@ def partial_1_J(a, b):
     J /=  len(data)
     return J
 
-def J(a, b):
-        temp = 0
-        for i in data:
-                temp += (a + b * i[0] - i[1]) ** 2
-        return temp / (2 * len(data))
-
 def main():
     data = np.array([[0.5, 0.9], [0.3, 0.7], [1.1, 2.3], [2.0, 4.3], [3.5, 6.8], [4.1, 8], [0, 0.1], [5.8, 11]])
 
+    # initialize variables at reasonable values
     theta_0 = np.arange(-3, 5, 0.1)
     theta_1 = np.arange(0, 3, 0.1)
     X, Y = np.meshgrid(theta_0, theta_1)
@@ -41,9 +45,11 @@ def main():
     theta_0 = theta_0 - alpha * partial_0_J(theta_0, theta_1)
     theta_1 = theta_1 - alpha * partial_1_J(temp, theta_1)
     current_J = J(theta_0, theta_1)
-
+    
+    # plot the loss curve
     surf = plt.contour(X, Y, Z, 40)
 
+    # find lowest loss
     while (current_J - prev_J) > min_dif:
         prev_J = current_J
         temp = theta_0
