b43b32cfa80bcbb14c2d6f3fedac1ba4690e6826
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2019-09-20 19:46:31 -0700

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -3,14 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#sample data
 data = np.array([[-1, 0], [-2, 0], [-1.5, 0], [1, 1], [2, 1], [1.2, 0], [-0.2, 1], [0, 1]])
 
+#error function for logistic equation: y = 1.0 / (1 + np.e **(-(a * x + b))
 def J(a, b):
     temp = 0
     for i in data:
             temp += (1.0 / (1 + np.e **(-(a * i[0] + b))) - i[1]) ** 2
     return temp / (2 * len(data))
-    
+
+#partial derivatives with respect to each of the variables
 def partial_a_J(a, b):
     partial = J(a, b) * (1 - J(a, b)) * a
     J /=  len(data)
@@ -22,6 +25,7 @@ def partial_b_J(a, b):
     return partial
 
 def main():
+    # range of values for a and b in logistic equation
     a = np.arange(-5, 5, 0.1)
     b = np.arange(-5, 5, 0.1)
     X, Y = np.meshgrid(a, b)
