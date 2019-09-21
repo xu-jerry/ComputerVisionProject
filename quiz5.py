b2a5d4ee85f523c141d6fbcfa497afdfee9b5579
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2019-09-20 20:26:08 -0700

    Update quiz5.py

--- quiz5.py
+++ quiz5.py
@@ -12,6 +12,7 @@ class KNearestNeighbors:
         self.ytr = y
 
     def predict(self, k, X):
+        #initiate the variables
         num_test = X.shape[0]
         Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
         min_indexes = np.array([])
@@ -20,9 +21,11 @@ class KNearestNeighbors:
             for j in xrange(num_test):
                 distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
                 min_index = np.argmin(distances)
+                # add the minimum indexes
                 min_indexes = min_indexes.append(min_indexes, min_index)
                 distances = np.remove(distances, min_index)
         
+        # find the mode of the minimum indexes
         min_index = stats.mode(min_indexes)        
         Ypred[i] = self.ytr[min_index]
         return Ypred
