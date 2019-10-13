4b3f56c2aff4d68ec636a9f68046347aadc0c8de
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2019-10-12 21:35:22 -0700

    Update crop_test.py

--- Traffic Light Project/crop_test.py
+++ Traffic Light Project/crop_test.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  7 20:12:36 2019
-
-@author: felix
-"""
-
 import numpy as np
 import cv2
 from read_label_file import get_all_labels
 
 images = get_all_labels('/home/felix/Downloads/Computer Vision Project/test.yaml')
-i = 24068
 
 for image in images:
-    
-    img = cv2.imread('/home/felix/Downloads/Computer Vision Project/rgb/test/'+ str(i) + '.png',1)
+    image_path = image['path']
+    image_id = image_path.split('/')[-1]
+    img = cv2.imread('/home/felix/Downloads/Computer Vision Project/rgb/test/' + image_id,1)
     
     for box in image['boxes']:
         y1 = int(box['y_min'])
@@ -27,22 +19,21 @@ for image in images:
             if not box['occluded']:
                 if box['label'] == 'Green':
                     crop_img = img[y1:y2, x1:x2]
-                    cv2.imwrite('/home/felix/Desktop/Cropped/Green/' + str(i) + '.jpg', crop_img)
+                    cv2.imwrite('/home/felix/Desktop/Cropped/Green/' + image_id, crop_img)
                     
                     cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-                    cv2.imwrite('/home/felix/Desktop/Cropped/Green/Reference/' + str(i) + 'annotated.jpg', img )
+                    cv2.imwrite('/home/felix/Desktop/Cropped/Green/Reference/' + image_id, img)
                 elif box['label'] == 'Red':
                     crop_img = img[y1:y2, x1:x2]
-                    cv2.imwrite('/home/felix/Desktop/Cropped/Red/' + str(i) + '.jpg', crop_img)
+                    cv2.imwrite('/home/felix/Desktop/Cropped/Red/' + image_id, crop_img)
                     
                     cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
-                    cv2.imwrite('/home/felix/Desktop/Cropped/Red/Reference/' + str(i) + 'annotated.jpg', img )
+                    cv2.imwrite('/home/felix/Desktop/Cropped/Red/Reference/' + image_id, img)
                 elif box['label'] == 'Yellow':
                     crop_img = img[y1:y2, x1:x2]
-                    cv2.imwrite('/home/felix/Desktop/Cropped/Yellow/' + str(i) + '.jpg', crop_img)
+                    cv2.imwrite('/home/felix/Desktop/Cropped/Yellow/' + image_id, crop_img)
                     
                     cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,255),2)
-                    cv2.imwrite('/home/felix/Desktop/Cropped/Yellow/Reference/' + str(i) + 'annotated.jpg', img )
-    i += 2
+                    cv2.imwrite('/home/felix/Desktop/Cropped/Yellow/Reference/' + image_id, img)
 
 cv2.destroyAllWindows()
